957ebc7f5d876fecdcee28cbcbb8331eb213a1b1
Paulo Cezar <paulo.souza6@estudante.ifms.edu.br>
2021-07-14 20:30:00 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,126 @@
+export const data = [
+    {
+      "id": 1,
+      "nome": "Cheese Burger",
+      "categoria":"Burger",
+      "ingredientes": "pão, molho da casa, hamburger 200g, alface, tomate, queijo",
+      "preco": 15.00,
+      "imagem": "hambuger/lanche1.png"
+    },
+    {
+        "id": 2,
+        "nome": "Hot Burger",
+        "categoria":"Burger",
+        "ingredientes": "pão, molho da casa, hamburger 200g, Salsicha, tomate, queijo",
+        "preco": 17.00,
+        "imagem": "hambuger/lanche2.png"
+    },
+    {
+        "id": 3,
+        "nome": "HOT DOG",
+        "categoria":"Burger",
+        "ingredientes": "pão, molho da casa, Salsicha, alface, tomate, queijo",
+        "preco": 10.00,
+        "imagem": "hambuger/lanche3.png"
+    },
+    {
+        "id": 4,
+        "nome": "X-Tudo",
+        "categoria":"Burger",
+        "ingredientes": "pão, molho da casa, hamburger 200g, salsicha, ovo,  alface, tomate, queijo",
+        "preco": 20.00,
+        "imagem": "hambuger/lanche4.png"
+    },
+    {
+        "id": 5,
+        "nome": "X-Americano",
+        "categoria":"Burger",
+        "ingredientes": "pão, molho da casa, hamburger 300g, alface, tomate, queijo",
+        "preco": 15.00,
+        "imagem": "hambuger/lanche6.png"
+    },
+    {
+        "id": 6,
+        "nome": "X-Salada",
+        "categoria":"Burger",
+        "ingredientes": "pão, maionese da casa, hamburger 300g, alface, tomate, queijo",
+        "preco": 17.00,
+        "imagem": "hambuger/lanche7.png"
+    },
+    {
+        "id": 7,
+        "nome": "HOT DOG Especial",
+        "categoria":"Burger",
+        "ingredientes": "pão, maionese da casa, salsicha, ovo, alface, tomate, queijo",
+        "preco": 25.00,
+        "imagem": "hambuger/lanche8.png"
+    },
+    {
+        "id": 8,
+        "nome": "Quatro Queijos",
+        "categoria":"pizza",
+        "ingredientes": "molho de tomate, muçarela, provolone, requeijão cremoso, parmesão, azeitonas, oregano",
+        "preco": 45.00,
+        "imagem": "pizza/pizza1.png"
+    },
+    {
+        "id": 9,
+        "nome": "Baiana",
+        "categoria":"pizza",
+        "ingredientes": "molho de tomate, muçarela, pimenta,calabreza, azeitonas, oregano",
+        "preco": 40.00,
+        "imagem": "pizza/pizza2.png"
+    },
+    {
+        "id": 10,
+        "nome": "Calabreza",
+        "categoria":"pizza",
+        "ingredientes": "molho de tomate, muçarela, calabreza, azeitonas, oregano",
+        "preco": 35.00,
+        "imagem": "pizza/pizza3.png"
+
+    },
+    {
+        "id": 10,
+        "nome": "Frango-Catupiry",
+        "categoria":"pizza",
+        "ingredientes": "molho de tomate, muçarela, Frango desfiado, catupiry, azeitonas, oregano",
+        "preco": 45.00,
+        "imagem": "pizza/pizza4.png"
+    },
+    {
+        "id": 11,
+        "nome": "Moda da Casa",
+        "categoria":"pizza",
+        "ingredientes": "molho de tomate, muçarela, Frango desfiado, catupiry, ovo, calabreza, azeitonas, oregano",
+        "preco": 50.00,
+        "imagem": "pizza/pizza5.png"
+
+    },
+    {
+        "id": 12,
+        "nome": "Portuguesa",
+        "categoria":"pizza",
+        "ingredientes": "molho de tomate, muçarela, Frango desfiado, catupiry,calabreza, azeitonas, oregano",
+        "preco": 48.00,
+        "imagem": "pizza/pizza6.png"
+    },
+    {
+      "id": 13,
+      "nome": "Burger Secreto",
+      "categoria":"Burger",
+      "ingredientes": "pão, molho da casa, hamburger 400g, alface, tomate, queijo",
+      "preco": 25.00,
+      "imagem": "hambuger/lanche5.png"
+    }
+];
+export const categorias = [
+    {
+        id: 1,
+        nome: 'Burger'
+    },
+    {
+        id:2,
+        nome: 'pizza'
+    },
+]
---
+++ main.js
@@ -0,0 +1,48 @@
+import { data } from './data.js'
+
+function listadomenu(id, nome, preco, imagem) {
+  return `
+  <li>
+     <a class="list" data-id="${id}">
+      <div class="menu-img">
+       <img src="${imagem}" alt="" width="65px" height="60px">
+        </div>
+         <span class="menu-name">${nome}</span>
+         <span class="menu-price">R$ ${preco}</span>
+         </a>
+              </li>
+    
+  `
+}
+
+function Cardapiomontado(categoriaSelected = null) {
+  const cardapio = document.querySelector('.menu-list')
+
+  const listaProdutosMenu = data.reduce(function (accumulator, { id, nome, preco, imagem, categoria }) {
+    if (!categoriaSelected) {
+      accumulator += listadomenu(id, nome, preco, imagem)
+    }
+    if (categoriaSelected === categoria) {
+      accumulator += listadomenu(id, nome, preco, imagem)
+    }
+    return accumulator
+  }, '')
+
+  cardapio.innerHTML = listaProdutosMenu
+
+}
+Cardapiomontado()
+
+const categorias = document.querySelectorAll('.menu_card')
+
+categorias.forEach(categoria => {
+  categoria.addEventListener('click', (event) => {
+
+    const categoriaSelected = event.currentTarget.dataset.categoria
+    Cardapiomontado(categoriaSelected)
+
+  })
+})
+
+
+
